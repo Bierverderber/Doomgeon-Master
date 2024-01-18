eaa25ee5ec02a6e416df8a154be196ee83d0431d
Арсен <arsenmkrt2006@mail.ru>
2024-01-18 22:55:15 +0300

    zhestko porabotal

--- main.py
+++ main.py
@@ -36,7 +36,9 @@ DoomGuy_sprite = pygame.sprite.Group()
 Cacodemon_sprite = pygame.sprite.Group()
 Bullets_sprites = pygame.sprite.Group()
 Aim_sprite = pygame.sprite.Group()
+EnemyBullets_sprites = pygame.sprite.Group()
 LetsGo = False
+Lose = False
 isMouseFocused = True
 
 
@@ -69,16 +71,23 @@ def boss_fight():   # Функция, отвечающая за битву с б
     screen.blit(fon, (0, 0))
 
     doomguy = DoomGuy()
-    Cacodemon()
+    cacodemon = Cacodemon()
     Aim()
+    health_bar = HealthBar(250, 650, 700, 20, 250)
+    doomguyhb = HealthBar(50, 50, 200, 20, 5)
 
     left = right = up = down = False
 
-    while True:
+    while not Lose:
+        if str(EnemyBullets_sprites)[7] == '0':
+            cacodemon.attack(EnemyBullet)
         screen.fill('black')
         screen.blit(fon, (0, 0))
         BossFight_sprites.draw(screen)
         Bullets_sprites.draw(screen)
+        EnemyBullets_sprites.draw(screen)
+        health_bar.draw(screen)
+        doomguyhb.draw(screen)
         if isMouseFocused:
             Aim_sprite.update(pygame.mouse.get_pos())
             Aim_sprite.draw(screen)
@@ -112,12 +121,14 @@ def boss_fight():   # Функция, отвечающая за битву с б
                     up = False
                 elif event.key in [pygame.K_DOWN, pygame.K_s]:
                     down = False
-        DoomGuy_sprite.update(left, right, up, down)
-        Bullets_sprites.update()
-        Cacodemon_sprite.update()
+        DoomGuy_sprite.update(left, right, up, down, doomguyhb)
+        Bullets_sprites.update(cacodemon, health_bar)
+        EnemyBullets_sprites.update(doomguy, doomguyhb)
+        Cacodemon_sprite.update(health_bar)
 
         pygame.display.flip()
         clock.tick(fps)
+    return
 
 
 class PlayButton(pygame.sprite.Sprite):
@@ -152,7 +163,8 @@ class DoomGuy(pygame.sprite.Sprite):
         self.rect.x = 100
         self.rect.y = 100
 
-    def update(self, left, right, up, down):
+    def update(self, left, right, up, down, doomguyhb):
+        global Lose
         if left:
             self.rect.x -= 300 / fps
         if right:
@@ -161,6 +173,9 @@ class DoomGuy(pygame.sprite.Sprite):
             self.rect.y -= 300 / fps
         if down:
             self.rect.y += 300 / fps
+        if doomguyhb.hp <= 0:
+            self.kill()
+            Lose = True
 
 
 class Cacodemon(pygame.sprite.Sprite):
@@ -174,7 +189,9 @@ class Cacodemon(pygame.sprite.Sprite):
                           random.randint(1, height - self.image.get_height()))
         self.v = 150
 
-    def update(self):
+        self.hp = 20
+
+    def update(self, health_bar):
         if self.positions[0] > self.rect.x:   # демон бегит
             x_move = 1
         else:
@@ -197,6 +214,40 @@ class Cacodemon(pygame.sprite.Sprite):
         else:
             self.positions = (random.randint(1, width - self.image.get_width()),
                               random.randint(1, height - self.image.get_height()))
+        if health_bar.hp <= 0:
+            self.kill()
+
+    def attack(self, EnemyBullet):
+        num_attack = random.randint(1, 3)
+        if num_attack == 1:
+            self.attack1(EnemyBullet)
+        elif num_attack == 2:
+            self.attack2(EnemyBullet)
+        elif num_attack == 3:
+            self.attack3(EnemyBullet)
+
+    def attack1(self, EnemyBullet):
+        for j in range(5):
+            for i in range(6):
+                EnemyBullet(i * (width // 6) + 30, -70 * j, 2)
+
+    def attack2(self, EnemyBullet):
+        for j in range(5):
+            for i in range(3):
+                EnemyBullet(- 70 * j, i * (height // 4) + 40, 1)
+
+    def attack3(self, EnemyBullet):
+        EnemyBullet1 = EnemyBullet(0, 0, -2)
+        EnemyBullet1.image = pygame.transform.scale(load_image('EnemyBullet.png'), (300, 300))
+        EnemyBullet1.rect = EnemyBullet1.image.get_rect()
+        EnemyBullet1.rect.x = 100
+        EnemyBullet1.rect.y = 800
+
+        EnemyBullet2 = EnemyBullet(0, 0, -2)
+        EnemyBullet2.image = pygame.transform.scale(load_image('EnemyBullet.png'), (300, 300))
+        EnemyBullet2.rect = EnemyBullet2.image.get_rect()
+        EnemyBullet2.rect.x = 700
+        EnemyBullet2.rect.y = 800
 
 
 class Aim(pygame.sprite.Sprite):
@@ -235,7 +286,7 @@ class Bullet(pygame.sprite.Sprite):
         else:
             self.y_move = -1
 
-    def update(self):
+    def update(self, cacodemon, health_bar):
         if (self.rect.x != self.need_pos[0] or self.rect.y != self.need_pos[1]) and self.flag:  # пули летают
             if (self.x_move == 1 and self.rect.x + (self.v / fps) * self.x_move >= self.need_pos[0]) \
                     or (self.x_move == -1 and self.rect.x + (self.v / fps) * self.x_move <= self.need_pos[0]):
@@ -253,8 +304,51 @@ class Bullet(pygame.sprite.Sprite):
                 self.rect.x = self.rect.x + (self.v / fps) * self.x_move
             else:
                 self.rect.y = self.rect.y + (self.v / fps) * self.y_move
+        if pygame.sprite.spritecollideany(self, Cacodemon_sprite):
+            if (cacodemon.rect.x + 70 < self.rect.x < cacodemon.rect.x + 210 and
+                    cacodemon.rect.y + 85 < self.rect.y < cacodemon.rect.y + 220):
+                self.kill()
+                health_bar.hp -= 1
+        if self.rect.x < -10 or self.rect.x > width + 10 or self.rect.y < -10 or self.rect.y > height + 10:
+            self.kill()
 
 
+class HealthBar():
+    def __init__(self, x, y, w, h, max_hp):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.hp = max_hp
+        self.max_hp = max_hp
+
+    def draw(self, surface):
+        if self.hp > 0:
+            ratio = self.hp / self.max_hp
+            pygame.draw.rect(surface, (180, 0, 0), (self.x, self.y, self.w, self.h))
+            pygame.draw.rect(surface, (0, 180, 0), (self.x, self.y, self.w * ratio, self.h))
+
+
+class EnemyBullet(pygame.sprite.Sprite):
+    def __init__(self, start_pos_x, start_pos_y, need_move):
+        super().__init__(all_sprites, EnemyBullets_sprites)
+        self.image = pygame.transform.scale(load_image('EnemyBullet.png'), (50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.x = start_pos_x
+        self.rect.y = start_pos_y
+        self.v = 180
+        self.need_move = need_move
+
+    def update(self, doomguy, doomguyhb):
+        if pygame.sprite.spritecollideany(self, DoomGuy_sprite):
+            self.kill()
+            doomguyhb.hp -= 1
+        if self.need_move in (-1, 1):
+            self.rect.x += (self.v // fps) * self.need_move
+        elif self.need_move in (-2, 2):
+            self.rect.y += (self.v // fps) * (self.need_move // 2)
+        if self.rect.x < -300 or self.rect.x > width + 300 or self.rect.y < -300 or self.rect.y > height + 300:
+            self.kill()
 
 
 
