beae6d1d15e60a57531d3420c02de0508ec25707
Арсен <arsenmkrt2006@mail.ru>
2024-01-25 19:47:07 +0300

    ifdtfsisptietfptfyl

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ PlayButton_sprite = pygame.sprite.Group()
 BossFight_sprites = pygame.sprite.Group()
 DoomGuy_sprite = pygame.sprite.Group()
 Cacodemon_sprite = pygame.sprite.Group()
+Rifle_sprite = pygame.sprite.Group()
 Bullets_sprites = pygame.sprite.Group()
 Aim_sprite = pygame.sprite.Group()
 EnemyBullets_sprites = pygame.sprite.Group()
@@ -75,13 +76,17 @@ def start_screen():   # функция, отвечающая за главное
 
 def boss_fight():   # Функция, отвечающая за битву с боссом
     global isMouseFocused
+    sound1 = pygame.mixer.Sound('data/DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOM.mp3')
+    sound1.play(-1)
+    sound1.set_volume(0.5)
     fon = pygame.transform.scale(load_image('hellfloor.png'), (width, height))
     screen.blit(fon, (0, 0))
 
-    doomguy = DoomGuy()
+    doomguy = DoomGuy(load_image("animationtry1.png"), load_image('reversedanimaaaaaaation.png'), 4, 1)
     cacodemon = Cacodemon()
-    Aim()
-    health_bar = HealthBar(250, 650, 700, 20, 5)
+    Rifle(doomguy)
+    aim = Aim()
+    health_bar = HealthBar(250, 650, 700, 20, 50)
     doomguyhb = HealthBar(50, 50, 200, 20, 5)
 
     left = right = up = down = False
@@ -91,7 +96,9 @@ def boss_fight():   # Функция, отвечающая за битву с б
             cacodemon.attack(EnemyBullet)
         screen.fill('black')
         screen.blit(fon, (0, 0))
-        BossFight_sprites.draw(screen)
+        DoomGuy_sprite.draw(screen)
+        Rifle_sprite.draw(screen)
+        Cacodemon_sprite.draw(screen)
         Bullets_sprites.draw(screen)
         EnemyBullets_sprites.draw(screen)
         health_bar.draw(screen)
@@ -129,10 +136,11 @@ def boss_fight():   # Функция, отвечающая за битву с б
                     up = False
                 elif event.key in [pygame.K_DOWN, pygame.K_s]:
                     down = False
-        DoomGuy_sprite.update(left, right, up, down, doomguyhb, cacodemon)
+        DoomGuy_sprite.update(left, right, up, down, doomguyhb, cacodemon, aim, sound1)
         Bullets_sprites.update(cacodemon, health_bar)
-        EnemyBullets_sprites.update(doomguy, doomguyhb)
+        EnemyBullets_sprites.update(doomguy, doomguyhb, ayayaya)
         Cacodemon_sprite.update(health_bar)
+        Rifle_sprite.update(doomguy, aim)
 
         pygame.display.flip()
         clock.tick(fps)
@@ -164,14 +172,19 @@ class Logo(pygame.sprite.Sprite):
 
 
 class DoomGuy(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, sheet, sheet2, columns, rows):
         super().__init__(DoomGuy_sprite, BossFight_sprites, all_sprites)
-        self.image = pygame.transform.scale(load_image('doom_GAY.png'), (60, 80))
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.frames2 = []
+        self.cut_sheet2(sheet2, columns, rows)
+        self.cur_frame = 0
+        self.image = pygame.transform.scale(self.frames[self.cur_frame], (60, 80))
         self.rect = self.image.get_rect()
         self.rect.x = 100
         self.rect.y = 100
 
-    def update(self, left, right, up, down, doomguyhb, cacodemon):
+    def update(self, left, right, up, down, doomguyhb, cacodemon, aim, sound1):
         global Lose
         if left:
             self.rect.x -= 300 / fps
@@ -181,7 +194,20 @@ class DoomGuy(pygame.sprite.Sprite):
             self.rect.y -= 300 / fps
         if down:
             self.rect.y += 300 / fps
+        if left or right or up or down:
+            if self.rect.x > aim.rect.x:
+                self.cur_frame = (self.cur_frame + 1) % len(self.frames2)
+                self.image = pygame.transform.scale(self.frames2[self.cur_frame], (60, 80))
+            else:
+                self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+                self.image = pygame.transform.scale(self.frames[self.cur_frame], (60, 80))
+        else:
+            if self.rect.x > aim.rect.x:
+                self.image = pygame.transform.scale(load_image('reverseddoomguy.png'), (60, 80))
+            else:
+                self.image = pygame.transform.scale(load_image('doom_GAY.png'), (60, 80))
         if doomguyhb.hp <= 0:
+            sound1.set_volume(0)
             play_music('data/Tyler1.mp3')
             self.kill()
             Lose = True
@@ -197,6 +223,24 @@ class DoomGuy(pygame.sprite.Sprite):
             if (cacodemon.rect.x + 70 < self.rect.x < cacodemon.rect.x + 210 and
                     cacodemon.rect.y + 85 < self.rect.y < cacodemon.rect.y + 220):
                 doomguyhb.hp -= 1
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def cut_sheet2(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames2.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
 
 
 class Cacodemon(pygame.sprite.Sprite):
@@ -242,12 +286,16 @@ class Cacodemon(pygame.sprite.Sprite):
             Win = True
 
     def attack(self, EnemyBullet):
+        global ayayaya
         num_attack = random.randint(1, 3)
         if num_attack == 1:
+            ayayaya = False
             self.attack1(EnemyBullet)
         elif num_attack == 2:
+            ayayaya = False
             self.attack2(EnemyBullet)
         elif num_attack == 3:
+            ayayaya = True
             self.attack3(EnemyBullet)
 
     def attack1(self, EnemyBullet):
@@ -265,7 +313,7 @@ class Cacodemon(pygame.sprite.Sprite):
         EnemyBullet1.image = pygame.transform.scale(load_image('EnemyBullet.png'), (300, 300))
         EnemyBullet1.rect = EnemyBullet1.image.get_rect()
         EnemyBullet1.rect.x = 50
-        EnemyBullet1.rect.y = 800
+        EnemyBullet1.rect.y = 1200
 
         EnemyBullet2 = EnemyBullet(0, 0, -2)
         EnemyBullet2.image = pygame.transform.scale(load_image('EnemyBullet.png'), (300, 300))
@@ -277,7 +325,7 @@ class Cacodemon(pygame.sprite.Sprite):
         EnemyBullet3.image = pygame.transform.scale(load_image('EnemyBullet.png'), (300, 300))
         EnemyBullet3.rect = EnemyBullet2.image.get_rect()
         EnemyBullet3.rect.x = 850
-        EnemyBullet3.rect.y = 800
+        EnemyBullet3.rect.y = 1200
 
 
 class Aim(pygame.sprite.Sprite):
@@ -360,6 +408,26 @@ class HealthBar():
             pygame.draw.rect(surface, (0, 180, 0), (self.x, self.y, self.w * ratio, self.h))
 
 
+class Rifle(pygame.sprite.Sprite):
+    def __init__(self, doomguy):
+        super().__init__(all_sprites, Rifle_sprite)
+        self.image = pygame.transform.scale(load_image('rifle.png'), (96, 30))
+        self.rect = self.image.get_rect()
+        self.rect.x = doomguy.rect.x + 10
+        self.rect.y = doomguy.rect.y + 50
+
+    def update(self, doomguy, aim):
+        self.rect.x = doomguy.rect.x - 10
+        self.rect.y = doomguy.rect.y + 50
+        if aim.rect.x < doomguy.rect.x:
+            self.image = pygame.transform.scale(load_image('reversedrifle.png'), (96, 30))
+            self.rect.x -= 25
+
+        else:
+            self.image = pygame.transform.scale(load_image('rifle.png'), (96, 30))
+            self.rect.x += 25
+
+
 class EnemyBullet(pygame.sprite.Sprite):
     def __init__(self, start_pos_x, start_pos_y, need_move):
         super().__init__(all_sprites, EnemyBullets_sprites)
@@ -370,7 +438,7 @@ class EnemyBullet(pygame.sprite.Sprite):
         self.v = 180
         self.need_move = need_move
 
-    def update(self, doomguy, doomguyhb):
+    def update(self, doomguy, doomguyhb, ayayaya):
         if pygame.sprite.spritecollideany(self, DoomGuy_sprite):
             play_music('data/aubolno.mp3')
             self.kill()
@@ -379,7 +447,11 @@ class EnemyBullet(pygame.sprite.Sprite):
             self.rect.x += (self.v // fps) * self.need_move
         elif self.need_move in (-2, 2):
             self.rect.y += (self.v // fps) * (self.need_move // 2)
-        if self.rect.x < -300 or self.rect.x > width + 300 or self.rect.y < -300 or self.rect.y > height + 300:
+        if ((self.rect.x < -300 or self.rect.x > width + 300 or self.rect.y < -300 or self.rect.y > height + 300)
+                and not ayayaya):
+            self.kill()
+        elif ((self.rect.x < -300 or self.rect.x > width + 300 or self.rect.y < -300 or self.rect.y > height + 1000)
+              and ayayaya):
             self.kill()
 
 
